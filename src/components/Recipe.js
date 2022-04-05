76fa407839418183be563082450a602fecc13926
Zachary Zhou <zzhou43@jhu.edu>
2022-04-05 16:17:08 -0400

    Fixed some API stuff

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ListRecipe from './ListRecipe';
 import {Link} from 'react-router-dom';
-const API = process.env.NODE_ENV === 'production' ? 'myapp.herokuapp.com' : 'http://localhost:5000';
+
+}
+const API = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000';
 
 
 function Recipe(props) {
